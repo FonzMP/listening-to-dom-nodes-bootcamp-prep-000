67b3978f13821b7b0dad4b07852ff3387711d4ca
Alex Wilson <fonzmp@gmail.com>
2018-04-19 20:17:14 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const lis = document.querySelectorAll('div');
+const buttonCreation = document.createElement('button');
+buttonCreation.innerHTML('delete');
 
+lis.appendChild(buttonCreation);
 console.log(lis);
